c39dac074f81f03802ada406894775f2c729bbfe
Sergei Zahharenko <sergei.zahharenko@riverty.com>
2025-10-13 12:44:54 +0300

    Death list

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,18 @@ const LogParser: React.FC = () => {
       /(.+) was cut in half by (.+?)'s chaingun/,
       /(.+) almost dodged (.+?)'s rocket/,
       /(.+) was blown away by (.+?)'s super shotgun/,
+      /(.+) was melted by (.+?)'s hyperblaster/,
+      /(.+) saw the pretty lights from (.+?)'s BFG/,
+      /(.+) was disintegrated by (.+?)'s BFG blast/,
+      /(.+) was blasted by (.+)/,
+      /(.+) was gunned down by (.+)/,
+      /(.+) was popped by (.+?)'s grenade/,
+      /(.+) was shredded by (.+?)'s shrapnel/,
+      /(.+) caught (.+?)'s handgrenade/,
+      /(.+) didn't see (.+?)'s handgrenade/,
+      /(.+) feels (.+?)'s pain/,
+      /(.+) tried to invade (.+?)'s personal space/,
+      /(.+) couldn't hide from (.+?)'s BFG/
     ];
     lines.forEach(line => {
       for (const pattern of killPatterns) {
